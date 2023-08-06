f496f875cbd90e25fc0615aed6106fc8f73a7681
rgoerv <roma.eremeev.2014@mail.ru>
2023-08-06 16:18:28 +0300

    before uninitialized_* functions save with test in main.cpp

--- Active Code/optional/optional.h
+++ Active Code/optional/optional.h
@@ -20,35 +20,116 @@ public:
         is_initialized_ = true;
     }
     Optional(T&& value) {
-        element = new(&data_[0]) T(std::move(value));
+        element = new(&data_[0]) T(std::forward<T>(value));
         is_initialized_ = true;
     }
     Optional(const Optional& other) {
-        
+        if(other.HasValue()) {
+            is_initialized_ = true;
+            element = new(&data_[0]) T(other.Value());
+        }
     }
-    Optional(Optional&& other);
+    Optional(Optional&& other) {
+        if(other.HasValue()) {
+            is_initialized_ = other.HasValue();
+            element = new(&data_[0]) T(std::forward<T>(other.Value()));
+        }
+    }
 
-    Optional& operator=(const T& value);
-    Optional& operator=(T&& rhs);
-    Optional& operator=(const Optional& rhs);
-    Optional& operator=(Optional&& rhs);
+    Optional& operator=(const T& value) {
+        if(is_initialized_) {
+            *element = value;
+        }
+        else {
+            element = new(&data_[0]) T(value);
+            is_initialized_ = true;
+        }
+        return *this;
+    }
+    Optional& operator=(T&& rhs) {
+        if(is_initialized_) {
+            *element = std::forward<T>(rhs);
+        }
+        else {
+            element = new(&data_[0]) T(std::forward<T>(rhs));
+            is_initialized_ = true;
+        }
+        return *this;
+    }
+    Optional& operator=(const Optional& rhs){
+        if(is_initialized_ && rhs.HasValue()) {
+            Value() = rhs.Value();
+            is_initialized_ = true;
+        }
+        else if(is_initialized_ && !rhs.HasValue()) {
+            Reset();
+        }
+        else if(!is_initialized_ && rhs.HasValue()) {
+            element = new(&data_[0]) T(rhs.Value());
+            is_initialized_ = true;
+        }
+        return *this;
+    }
+    Optional& operator=(Optional&& rhs) {
+        if(is_initialized_ && rhs.HasValue()) {
+            Value() = std::forward<T>(rhs.Value());
+            is_initialized_ = true;
+        }
+        else if(is_initialized_ && !rhs.HasValue()) {
+            Reset();
+        }
+        else if(!is_initialized_ && rhs.HasValue()) {
+            element = new(&data_[0]) T(std::forward<T>(rhs.Value()));
+            is_initialized_ = true;
+        }
+        return *this;
+    }
 
-    ~Optional();
+    ~Optional() {
+        if(is_initialized_) {
+            element->~T();
+        }   
+    }
 
-    bool HasValue() const;
+    bool HasValue() const {
+        return is_initialized_;
+    }
 
     // Операторы * и -> не должны делать никаких проверок на пустоту Optional.
     // Эти проверки остаются на совести программиста
-    T& operator*();
-    const T& operator*() const;
-    T* operator->();
-    const T* operator->() const;
+    T& operator*(){
+        return *element;
+    }
+    const T& operator*() const {
+        return *element;
+    }
+    T* operator->() {
+        return element;
+    }
+    const T* operator->() const {
+        return element;
+    }
 
     // Метод Value() генерирует исключение BadOptionalAccess, если Optional пуст
-    T& Value();
-    const T& Value() const;
+    T& Value() {
+        if(is_initialized_) {
+            return *element;
+        }
+        throw BadOptionalAccess();
+    }
+    const T& Value() const {
+        if(is_initialized_) {
+            return *element;
+        }
+        throw BadOptionalAccess();
+    }
 
-    void Reset();
+    void Reset() {
+        if(is_initialized_) {
+            element->~T();
+        }
+        is_initialized_ = false;
+    }
 
 private:
     // alignas нужен для правильного выравнивания блока памяти
